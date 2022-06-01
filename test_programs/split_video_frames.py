ba278c539c5e02830d2d660df2bc78492ef6ca33
Sujith Christopher <50609700+SujithChristopher@users.noreply.github.com>
2022-06-01 10:40:31 +0530

    Initial commit

--- test_programs/split_video_frames.py
+++ test_programs/split_video_frames.py
@@ -1,11 +1,12 @@
 import cv2
 
-cap = cv2.VideoCapture("aruco_original.mp4")
+cap = cv2.VideoCapture("..//video_recordings//phonecam_60fps.mp4")
+# cap = cv2.VideoCapture(r"C:\Users\CMC\Documents\openposelibs\pose\skateboard_gui\video_recordings\phonecam_60fps.mp4")
 count = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
-        cv2.imwrite(f"images//{count}.jpg", frame)
+        cv2.imwrite(f".//images//{count}.jpg", frame)
         count += 1
         print(count)
     else:
